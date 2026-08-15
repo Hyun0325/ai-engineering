d8d63f525bd2f0d4c8acf66ef5a5bb00a862f66b
승재 <hjsa0631@gmail.com>
2026-08-15 16:19:56 +0900

    Update data_loader typo

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,9 +1,17 @@
 import pandas as pd
 
 def load_temperature_data():
-
-    print("Loading temperaturedata...")
-
     data = pd.read_csv("data/temperature.csv")
+
+    data.columns = data.columns.str.strip()
+
+    print("Loading temperature data...")
+    print(data)
+    print(data.columns)
+    print(data["temperature"])
+ 
+    print("Maximum temperature:", data["temperature"].max())
+    print("Minimum temperature:", data["temperature"].min())
+    print("Average temperature:", data["temperature"].mean())
 
     return data
